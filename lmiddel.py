ce9189acd31f8a4870f3d04c49f5ae89907b756a
sorenjo <serenjohanse@yahoo.com>
2020-09-29 08:48:37 +0200

    Flere detaljer

--- lmiddel.py
+++ lmiddel.py
@@ -1,1 +1,23 @@
+from flask import Flask
 from flask import render_template
+from flask import request
+from flask import url_for
+from data import Data
+import csv
+import os
+
+app = Flask(__name__)
+
+def gen_opg():
+    Data()
+
+
+@app.route("/")
+@app.route("/index")
+@app.route("/index.html")
+def index():
+    return render_template("index.htmltemplate")
+
+@app.route("/opgave/<opgnr>")
+def opgave(opgnr):
+    
